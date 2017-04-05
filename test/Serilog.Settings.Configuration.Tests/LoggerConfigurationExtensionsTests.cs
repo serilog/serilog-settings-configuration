24c67508b0ff5bb52d4e6a52c70cfa36935bd687
Sergey Komisarchik <komisarchik@media-saturn.com>
2017-04-05 12:38:14 +0300

    fixes #957 https://github.com/serilog/serilog/issues/957

---
+++ test/Serilog.Settings.Configuration.Tests/LoggerConfigurationExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using Xunit;
+
+namespace Serilog.Settings.Configuration.Tests
+{
+    public class LoggerConfigurationExtensionsTests
+    {
+        [Fact]
+        public void ReadFromConfigurationShouldNotThrowOnEmptyConfiguration()
+        {
+            Action act = () => new LoggerConfiguration().ReadFrom.Configuration(new ConfigurationBuilder().Build());
+
+            // should not throw
+            act();
+        }
+    }
+}
