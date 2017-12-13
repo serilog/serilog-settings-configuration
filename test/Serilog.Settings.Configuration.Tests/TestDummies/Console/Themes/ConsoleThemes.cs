c6ec5b44215ec6a820b75c5b0be5fd8ea44257ab
Douglas Money <douglasrmoney@gmail.com>
2017-12-13 00:51:02 -0500

    Support for static member as the value of setting using `::`

---
+++ test/Serilog.Settings.Configuration.Tests/TestDummies/Console/Themes/ConsoleThemes.cs
@@ -0,0 +1,7 @@
+﻿namespace Serilog.Settings.Configuration.Tests.TestDummies.Console.Themes
+{
+    public static class ConsoleThemes
+    {
+        public static ConsoleTheme Theme1 { get; } = new ConcreteConsoleTheme();
+    }
+}
