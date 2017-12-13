116d0e63b74555735f485069363e2162576f85e6
Douglas Money <douglasrmoney@gmail.com>
2017-12-13 00:51:02 -0500

    Support for static member as the value of setting using `::`

---
+++ test/Serilog.Settings.Configuration.Tests/Support/CustomConsoleTheme.cs
@@ -0,0 +1,8 @@
+﻿using Serilog.Settings.Configuration.Tests.TestDummies.Console.Themes;
+
+namespace Serilog.Settings.Configuration.Tests.Support
+{
+    class CustomConsoleTheme : ConsoleTheme
+    {
+    }
+}
---
+++ test/Serilog.Settings.Configuration.Tests/TestDummies/Console/DummyConsoleSink.cs
@@ -0,0 +1,22 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+using Serilog.Settings.Configuration.Tests.TestDummies.Console.Themes;
+using System;
+
+namespace Serilog.Settings.Configuration.Tests.TestDummies.Console
+{
+    public class DummyConsoleSink : ILogEventSink
+    {
+        public DummyConsoleSink(ConsoleTheme theme = null)
+        {
+            Theme = theme ?? ConsoleTheme.None;
+        }
+
+        [ThreadStatic]
+        public static ConsoleTheme Theme;
+
+        public void Emit(LogEvent logEvent)
+        {
+        }
+    }
+}
