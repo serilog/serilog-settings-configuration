e0ac44f88a595a2e768ad8652d5f5c83836228b5
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2018-03-16 06:56:02 +1000

    Fixes #91 - wrong parameter names on surrogate WriteTo.Logger() configuration method

---
+++ src/Serilog.Settings.Configuration/Settings/Configuration/ConfigurationValueSyntax.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Serilog.Settings.Configuration
+{
+    static class ConfigurationValueSyntax
+    {
+        const string LevelSwitchNameRegex = @"^\$[A-Za-z]+[A-Za-z0-9]*$";
+
+        public static bool IsValidSwitchName(string input)
+        {
+            return Regex.IsMatch(input, LevelSwitchNameRegex);
+        }
+    }
+}
