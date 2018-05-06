76b0bf2614ecf7cc222c21325021e437fede550b
Jon McGuire <mcguirev10@gmail.com>
2018-05-06 11:15:46 -0400

    support the basic destructure settings

--- sample/Sample/Program.cs
+++ sample/Sample/Program.cs
@@ -32,12 +32,23 @@ namespace Sample
                 logger.ForContext(Constants.SourceContextPropertyName, "Microsoft").Error("Hello, world!");
                 logger.ForContext(Constants.SourceContextPropertyName, "MyApp.Something.Tricky").Verbose("Hello, world!");
 
-                Console.WriteLine();
+                logger.Information("Destructure with max object nesting depth:\n{@NestedObject}",
+                    new { FiveDeep = new { Two = new { Three = new { Four = new { Five = "the end" } } } } });
+
+                logger.Information("Destructure with max string length:\n{@LongString}",
+                    new { TwentyChars = "0123456789abcdefghij" });
+
+                logger.Information("Destructure with max collection count:\n{@BigData}",
+                    new { TenItems = new string[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" } });
+
+                Console.WriteLine("\nPress \"q\" to quit, or any other key to run again.\n");
             }
             while (!args.Contains("--run-once") && (Console.ReadKey().KeyChar != 'q'));
         }
     }
 
+    // The filter syntax in the sample configuration file is
+    // processed by the Serilog.Filters.Expressions package.
     public class CustomFilter : ILogEventFilter
     {
         public bool IsEnabled(LogEvent logEvent)
