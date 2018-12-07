2caaed7be40fb09c6327b5f16a47a186e04df96d
Jan Zahradník <jan.zahradnik@data3s.com>
2018-12-07 08:40:33 +0100

    parameters

--- src/Serilog.Settings.Configuration/Settings/Configuration/ConfigurationReader.cs
+++ src/Serilog.Settings.Configuration/Settings/Configuration/ConfigurationReader.cs
@@ -375,7 +375,10 @@ namespace Serilog.Settings.Configuration
 
             if (selectedMethod == null)
             {
-                var methodsByName = candidateMethods.Where(m => m.Name == name).ToList();
+                var methodsByName = candidateMethods
+                    .Where(m => m.Name == name)
+                    .Select(m => $"{m.Name}({string.Join(", ", m.GetParameters().Skip(1).Select(p => p.Name))}")
+                    .ToList();
                 if (!methodsByName.Any())
                     throw new MissingMethodException($"Unable to find a method called {name}. Candidate methods are:{Environment.NewLine}{string.Join(Environment.NewLine, candidateMethods)}");
 
