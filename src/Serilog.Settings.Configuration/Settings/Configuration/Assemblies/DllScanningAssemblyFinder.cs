297a13229e00523facd7941c6f2ec209936919a2
Sergey Komisarchik <sek@bright-box.com>
2019-01-04 23:14:35 +0300

    DllScanningAssemblyFinder fixes: - ArgumentNullException fix when BaseDirectory is null - additional paths probing:  AppDomainSetup.PrivateBinPath, current assembly location path when BaseDirectory is null

--- src/Serilog.Settings.Configuration/Settings/Configuration/Assemblies/DllScanningAssemblyFinder.cs
+++ src/Serilog.Settings.Configuration/Settings/Configuration/Assemblies/DllScanningAssemblyFinder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 
@@ -9,12 +10,57 @@ namespace Serilog.Settings.Configuration.Assemblies
     {
         public override IReadOnlyList<AssemblyName> FindAssembliesContainingName(string nameToFind)
         {
-            var query = from outputAssemblyPath in System.IO.Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll")
-                let assemblyFileName = System.IO.Path.GetFileNameWithoutExtension(outputAssemblyPath)
-                where IsCaseInsensitiveMatch(assemblyFileName, nameToFind)
-                select AssemblyName.GetAssemblyName(outputAssemblyPath);
+            var probeDirs = new List<string>();
+            
+            if (!string.IsNullOrEmpty(AppDomain.CurrentDomain.BaseDirectory))
+            {
+                probeDirs.Add(AppDomain.CurrentDomain.BaseDirectory);
+
+#if PRIVATE_BIN
+                var privateBinPath = AppDomain.CurrentDomain.SetupInformation.PrivateBinPath;
+                if (!string.IsNullOrEmpty(privateBinPath))
+                {
+                    foreach (var path in privateBinPath.Split(';'))
+                    {
+                        if (Path.IsPathRooted(path))
+                        {
+                            probeDirs.Add(path);
+                        }
+                        else
+                        {
+                            probeDirs.Add(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path));
+                        }
+                    }
+                }
+#endif
+            }
+            else
+            {
+                probeDirs.Add(Path.GetDirectoryName(typeof(AssemblyFinder).Assembly.Location));
+            }
+
+            var query = from probeDir in probeDirs
+                        where Directory.Exists(probeDir)
+                        from outputAssemblyPath in Directory.GetFiles(probeDir, "*.dll")
+                        let assemblyFileName = Path.GetFileNameWithoutExtension(outputAssemblyPath)
+                        where IsCaseInsensitiveMatch(assemblyFileName, nameToFind)
+                        let assemblyName = TryGetAssemblyNameFrom(outputAssemblyPath)
+                        where assemblyName != null
+                        select assemblyName;
 
             return query.ToList().AsReadOnly();
+
+            AssemblyName TryGetAssemblyNameFrom(string path)
+            {
+                try
+                {
+                    return AssemblyName.GetAssemblyName(path);
+                }
+                catch (BadImageFormatException)
+                {
+                    return null;
+                }
+            }
         }
     }
 }
---
+++ test/Serilog.Settings.Configuration.Tests/DllScanningAssemblyFinderTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+
+using Xunit;
+
+using Serilog.Settings.Configuration.Assemblies;
+
+namespace Serilog.Settings.Configuration.Tests
+{
+    public class DllScanningAssemblyFinderTests : IDisposable
+    {
+        readonly string _privateBinPath;
+
+        public DllScanningAssemblyFinderTests()
+        {
+            var d1 = GetOrCreateDirectory("bin1");
+            var d2 = GetOrCreateDirectory("bin2");
+            var d3 = GetOrCreateDirectory("bin3");
+
+            _privateBinPath = $"{d1.Name};{d2.FullName};{d3.Name}";
+
+            DirectoryInfo GetOrCreateDirectory(string name)
+                => Directory.Exists(name) ? new DirectoryInfo(name) : Directory.CreateDirectory(name);
+        }
+
+        public void Dispose()
+        {
+            Directory.Delete("bin1", true);
+            Directory.Delete("bin2", true);
+            Directory.Delete("bin3", true);
+        }
+
+        [Fact]
+        public void ShouldProbeCurrentDirectory()
+        {
+            var assemblyNames = new DllScanningAssemblyFinder().FindAssembliesContainingName("testdummies");
+            Assert.Single(assemblyNames);
+        }
+
+#if PRIVATE_BIN
+        [Fact]
+        public void ShouldProbePrivateBinPath()
+        {
+            File.Copy("testdummies.dll", "bin1/customSink1.dll", true);
+            File.Copy("testdummies.dll", "bin2/customSink2.dll", true);
+            File.Copy("testdummies.dll", "bin3/thirdpartydependency.dll", true);
+
+            var ad = AppDomain.CreateDomain("serilog", null,
+                new AppDomainSetup
+                {
+                    ApplicationBase = AppDomain.CurrentDomain.BaseDirectory,
+                    PrivateBinPath = _privateBinPath
+                });
+
+            try
+            {
+                ad.DoCallBack(DoTestInner);
+            }
+            finally
+            {
+                AppDomain.Unload(ad);
+            }
+
+            void DoTestInner()
+            {
+                var assemblyNames = new DllScanningAssemblyFinder().FindAssembliesContainingName("customSink");
+                Assert.Equal(2, assemblyNames.Count);
+            }
+        }
+#endif
+    }
+}
