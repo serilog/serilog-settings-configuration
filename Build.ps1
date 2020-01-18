56ae87ef9d02e87e03b5956fd4b3a33540f6836e
TeBeCo <tebeco@gmail.com>
2020-01-18 20:32:39 +0100

    Remove linter warning for powershell script

--- Build.ps1
+++ Build.ps1
@@ -1,35 +1,35 @@
-echo "build: Build started"
+Write-Output "build: Build started"
 
 Push-Location $PSScriptRoot
 
 if(Test-Path .\artifacts) {
-	echo "build: Cleaning .\artifacts"
+	Write-Output "build: Cleaning .\artifacts"
 	Remove-Item .\artifacts -Force -Recurse
 }
 
 & dotnet restore --no-cache
 
-$branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
-$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$null -ne $env:APPVEYOR_REPO_BRANCH ];
+$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$null -ne $env:APPVEYOR_BUILD_NUMBER];
 $suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
 
-echo "build: Version suffix is $suffix"
+Write-Output "build: Version suffix is $suffix"
 
-foreach ($src in ls src/*) {
+foreach ($src in Get-ChildItem "src/*") {
     Push-Location $src
 
-	echo "build: Packaging project in $src"
+	Write-Output "build: Packaging project in $src"
 
     & dotnet pack -c Release -o ..\..\artifacts --version-suffix=$suffix --include-source
-    if($LASTEXITCODE -ne 0) { exit 1 }    
+    if($LASTEXITCODE -ne 0) { exit 1 }
 
     Pop-Location
 }
 
-foreach ($test in ls test/*.PerformanceTests) {
+foreach ($test in Get-ChildItem "test/*.PerformanceTests") {
     Push-Location $test
 
-	echo "build: Building performance test project in $test"
+	Write-Output "build: Building performance test project in $test"
 
     & dotnet build -c Release
     if($LASTEXITCODE -ne 0) { exit 2 }
@@ -37,10 +37,10 @@ foreach ($test in ls test/*.PerformanceTests) {
     Pop-Location
 }
 
-foreach ($test in ls test/*.Tests) {
+foreach ($test in Get-ChildItem "test/*.Tests") {
     Push-Location $test
 
-	echo "build: Testing project in $test"
+	Write-Output "build: Testing project in $test"
 
     & dotnet test -c Release
     if($LASTEXITCODE -ne 0) { exit 3 }
