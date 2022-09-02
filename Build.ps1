455da0107c090b70a7b6bb2b5a2df3ef8bafaf7d
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2022-09-03 07:49:10 +1000

    Update build script with hopes of running consistently on all platforms

--- Build.ps1
+++ Build.ps1
@@ -2,9 +2,9 @@ echo "build: Build started"
 
 Push-Location $PSScriptRoot
 
-if (Test-Path .\artifacts) {
-    echo "build: Cleaning .\artifacts"
-    Remove-Item .\artifacts -Force -Recurse
+if(Test-Path .\artifacts) {
+	echo "build: Cleaning .\artifacts"
+	Remove-Item .\artifacts -Force -Recurse
 }
 
 & dotnet restore --no-cache
@@ -12,56 +12,47 @@ if (Test-Path .\artifacts) {
 $branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
 $revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "main" -and $revision -ne "local"]
+$commitHash = $(git rev-parse --short HEAD)
+$buildSuffix = @{ $true = "$($suffix)-$($commitHash)"; $false = "$($branch)-$($commitHash)" }[$suffix -ne ""]
 
-echo "build: Version suffix is $suffix"
+echo "build: Package version suffix is $suffix"
+echo "build: Build version suffix is $buildSuffix"
 
-foreach ($src in dir src/*) {
+foreach ($src in ls src/*) {
     Push-Location $src
 
-    echo "build: Packaging project in $src"
+	echo "build: Packaging project in $src"
 
-    & dotnet pack -c Release -o ..\..\artifacts --version-suffix=$suffix -p:ContinuousIntegrationBuild=true
-    if ($LASTEXITCODE -ne 0) { exit 1 }
+    & dotnet build -c Release --version-suffix=$buildSuffix
+
+    if($suffix) {
+        & dotnet pack -c Release --include-source --no-build -o ../../artifacts --version-suffix=$suffix  -p:ContinuousIntegrationBuild=true
+    } else {
+        & dotnet pack -c Release --include-source --no-build -o ../../artifacts  -p:ContinuousIntegrationBuild=true
+    }
+    if($LASTEXITCODE -ne 0) { exit 1 }
 
     Pop-Location
 }
 
-foreach ($test in dir test/*.PerformanceTests) {
+foreach ($test in ls test/*.Tests) {
     Push-Location $test
 
-    echo "build: Building performance test project in $test"
+	echo "build: Testing project in $test"
+
+    & dotnet test -c Release
+    if($LASTEXITCODE -ne 0) { exit 3 }
+
+    Pop-Location
+}
+
+foreach ($test in ls test/*.PerformanceTests) {
+    Push-Location $test
+
+	echo "build: Building performance test project in $test"
 
     & dotnet build -c Release
-    if ($LASTEXITCODE -ne 0) { exit 2 }
-
-    Pop-Location
-}
-
-foreach ($test in dir test/*.Tests) {
-    Push-Location $test
-
-    echo "build: Testing project in $test"
-
-    if ($PSVersionTable.Platform -eq "Unix") {
-        & dotnet test -c Release -f netcoreapp3.1
-        & dotnet test -c Release -f net6.0
-    } else {
-        & dotnet test -c Release
-    }
-
-    if ($LASTEXITCODE -ne 0) { exit 3 }
-
-    Pop-Location
-}
-
-if ($PSVersionTable.Platform -eq "Unix") {
-    Push-Location sample/Sample
-
-    & dotnet run -f netcoreapp3.1 -c Release --run-once
-    if ($LASTEXITCODE -ne 0) { exit 4 }
-
-    & dotnet run -f net6.0        -c Release --run-once
-    if ($LASTEXITCODE -ne 0) { exit 4 }
+    if($LASTEXITCODE -ne 0) { exit 2 }
 
     Pop-Location
 }
