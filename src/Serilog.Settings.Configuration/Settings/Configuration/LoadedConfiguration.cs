c58ef69e085c974e22621d233133543b2d30351a
Cédric Luthi <cedric.luthi@gmail.com>
2023-02-08 08:34:39 +0100

    Fix typo in documentation

--- src/Serilog.Settings.Configuration/Settings/Configuration/LoadedConfiguration.cs
+++ src/Serilog.Settings.Configuration/Settings/Configuration/LoadedConfiguration.cs
@@ -15,6 +15,6 @@ public class LoadedConfiguration
     /// <summary>
     /// The log level switches that were loaded from the <c>LevelSwitches</c> section of the configuration.
     /// </summary>
-    /// <remarks>The keys of the dictionary are the name of the switches, including the leading <c>$</c> character.</remarks>
+    /// <remarks>The keys of the dictionary are the names of the switches, including the leading <c>$</c> character.</remarks>
     public IReadOnlyDictionary<string, LoggingLevelSwitch> LogLevelSwitches { get; }
 }
